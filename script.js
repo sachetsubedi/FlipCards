b16f9f106c3f12ec5671733c24699da4770f029b
Sachet Subedi <sachetsubedi001@gmail.com>
2023-06-16 16:46:48 +0545

    Logic completed i think

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let container = document.getElementById("main");
 let boxes = document.querySelectorAll("#boxes");
-let numberArrey = [1, 2, 3, 4, 5, 6, 7, 8, 9,1, 2, 3, 4, 5, 6, 7, 8, 9];
-var boxDisplaing=0;
+let numberArrey = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var boxDisplaing = 0;
+var completedBox=0;
+
+
 boxes.forEach((box) => {
   box.style.backgroundColor = "red";
 });
@@ -14,27 +17,25 @@ function suffle(array) {
   }
   return array;
 }
-function displayText(){
-    boxes.forEach((box)=>{
-        box.addEventListener('click',()=>{
-            if(boxDisplaing<2 && box.value!=0){
-                box.innerHTML=box.value;
-                boxDisplaing++;
-               
+function displayText() {
+  boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+      if (boxDisplaing < 2 && box.value != 0) {
+        box.innerHTML = box.value;
+        boxDisplaing++;
+      }
+      if (boxDisplaing >= 2) {
+        setTimeout(function () {
+          boxes.forEach((box) => {
+            if (box.value != 0) {
+              box.innerHTML = "";
             }
-            if(boxDisplaing>=2){
-                setTimeout(function(){
-                    boxes.forEach((box)=>{
-                        if(box.value!=0){
-                            box.innerHTML='';
-                        }
-                    })
-                    boxDisplaing=0;
-                },300)
-            }
-            
-        })
-    })
+          });
+          boxDisplaing = 0;
+        }, 300);
+      }
+    });
+  });
 }
 
 displayText();
@@ -42,7 +43,7 @@ displayText();
 let shuffledArray = suffle(numberArrey);
 var i = 0;
 boxes.forEach((box) => {
- // box.innerHTML = shuffledArray[i];
+  // box.innerHTML = shuffledArray[i];
   box.value = shuffledArray[i];
   i++;
 });
@@ -51,27 +52,58 @@ boxes.forEach((box) => {
 var clickArr = [];
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
-    clickArr.push(box.value);
-    if (clickArr.length >= 2) {
-      console.log(clickArr);
-      check(clickArr);
-      clickArr = [];
+    if (box.dataset.clicked=="false") {
+      box.dataset.clicked = "true";
+      clickArr.push(box.value);
+      if (clickArr.length >= 2) {
+        console.log(clickArr);
+        check(clickArr);
+        clickArr = [];
+      }
     }
+    else{
+        clickArr.push(200);
+        check(clickArr);
+        clickArr = [];
+    }
+  
   });
 });
 
 //to store the clicked value and check
 
 function check(array) {
-    if(array[0]==array[1]){
-        boxes.forEach((box)=>{
-            if(box.value==array[0]){
-                box.innerHTML='X';
-                box.value=0;
-            }
-        })
-    }
-    else{
-        console.log("false");
-    }
+  if (array[0] == array[1]) {
+    boxes.forEach((box) => {
+      if (box.value == array[0]) {
+        completedBox++;
+        
+        box.innerHTML = "X";
+        box.value = 0;
+        
+
+      }
+    });
+  } else {
+    console.log("false");
+    clickArr = [];
+    setTimeout(function () {
+        boxes.forEach((box) => {
+          if (box.value != 0) {
+            box.innerHTML = "";
+          }
+        });
+        boxDisplaing = 0;
+      }, 300);
+    boxes.forEach((box)=>{
+        box.dataset.clicked="false";
+    })
+  }
+if(completedBox==boxes.length)
+  {
+    var display=document.getElementById('display');
+    display.innerText="You won";
+  }
 }
+
+
